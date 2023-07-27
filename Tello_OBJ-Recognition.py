1627c10f56dd7ac1f9cfb37763d430b05670d826
SJ99677 <140622119+SJ99677@users.noreply.github.com>
2023-07-27 17:06:47 -0400

    Update Tello_OBJ-Recognition.py
    clean up the code, add comments, etc

--- Tello_OBJ-Recognition.py
+++ Tello_OBJ-Recognition.py
@@ -1,3 +1,6 @@
+"""
+Written by Sam Johnson
+"""
 
 import cv2
 from djitellopy import tello
@@ -6,39 +9,29 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import time
 
-thres = 0.50
-nmsThres = 0.2
+accuracy = 0.50  # accuracy % (.5 default)
+nmAccuracy = 0.2  # removes duplicates (.2 default)
 
-# Code for webcam
-# cap = cv2.VideoCapture(0)
-# cap.set(3, 640)
-# cap.set(4, 480)
+classObj = []
+# List of potential objects that could be recognized
+fileList = 'ss.names'
+with open(fileList, 'rt') as f:
+    classObj = f.read().split('\n')
 
-classNames = []
-classFile = 'ss.names' # Contains a totoal of 91 different objects which can be recognized by the code
-with open(classFile, 'rt') as f:
-    classNames = f.read().split('\n')
-
-
-print(classNames)
-configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+print(classObj)
 weightsPath = "frozen_inference_graph.pb"
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 
-net = cv2.dnn_DetectionModel(weightsPath, configPath)
-net.setInputSize(320, 320)
-net.setInputScale(1.0 / 127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
+pic = cv2.dnn_DetectionModel(weightsPath, configPath)
+pic.setInputSize(320, 320)
+pic.setInputScale(1.0 / 127.5)
+pic.setInputMean((127.5, 127.5, 127.5))
+pic.setInputSwapRB(True)
 
+# defines graph
 graph = nx.Graph()
-# """
-# graph.add_node("Location A", pos=(0, 0))
-# graph.add_node("Location B", pos=(1, 1))
-# graph.add_node("Location C", pos=(2, 0))
-# graph.add_node("Location D", pos=(1, -1))
-# graph.add_node("Location E", pos=(0, 0))
-#             """
 
+# coordinates may be adjusted to change grid pattern. Default positions follow a triangle pattern.
 graph.add_node("Location A", pos=(0, 0))
 graph.add_node("Location B", pos=(-1, 0))
 graph.add_node("Location C", pos=(0, 2))
@@ -50,32 +43,32 @@ graph.add_edge("Location B", "Location C")
 graph.add_edge("Location C", "Location D")
 graph.add_edge("Location D", "Location E")
 graph.add_edge("Location E", "Location A")
+
 nx.draw(graph, pos=nx.get_node_attributes(graph, "pos"), with_labels=True)
 plt.show()
 
 # Plan the path
-# path = nx.dijkstra_path(graph, "Location A", "Location C")
 path = ["Location A", "Location B", "Location C", "Location D", "Location E"]
 
-me = tello.Tello()
-me.connect()
-print(me.get_battery())
-me.streamoff()
-me.streamon()
+drone = tello.Tello()
+drone.connect()
+print(drone.get_battery())
+drone.streamoff()
+drone.streamon()
 
-me.takeoff()
-#me.move_up(90)
-me.go_xyz_speed(0, 0, 105, 30)
+drone.takeoff()
+# drone moves up, 105cm to have higher view of surroundings
+drone.go_xyz_speed(0, 0, 105, 30)
 
-
+# Loop to draw rectangle around objects
 while True:
-    # success, img = cap.read()
-    img = me.get_frame_read().frame
-    classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=nmsThres) # To remove duplicates / declare accuracy
+    feed = drone.get_frame_read().frame
+    # To remove duplicates / declare accuracy
+    objIds, flat, boundB = pic.detect(feed, confThreshold=accuracy, nmsThreshold=nmAccuracy)
     try:
-        for classId, conf, box in zip(classIds.flatten(), confs.flatten(), bbox):
-            objName = classNames[classId - 1].lower()
-            distance = me.get_distance_tof()
+        for objId, flat, rectangle in zip(objIds.flatten(), flat.flatten(), boundB):
+            objName = classObj[objId - 1].lower()
+            distance = drone.get_distance_tof()
 
             # Initialize the current position
             current_position = (0, 0)
@@ -92,50 +85,37 @@ while True:
                 current_position = target_position
                 # Convert relative distance to centimeters
                 relative_distance_cm = (
-                    int(relative_distance[0] * 25),
-                    int(relative_distance[1] * 25)
+                    #change the multiplying variable to increase distance (50cm default)
+                    int(relative_distance[0] * 50),
+                    int(relative_distance[1] * 50)
                 )
-                # Send commands to the Tello to navigate to the current location
-                me.send_rc_control(relative_distance_cm[0], relative_distance_cm[1], 0, 0)
-                print("about to sleep")
-                time.sleep(5)
-                print("ending sleep")
-            while objName != "stop sign" or distance >= 130:
-                print("check1")
-                me.go_xyz_speed(60, 0, 0, 50)
-                me.sleep(4)
-            else:
-                print("check2")
-                me.send_rc_control(0, 0, 0, 0)
-                #me.sleep(2)
-                print("distance less than 200 cm, commencing landing")
-                # Center the frame on the object
-                # me.send_rc_control(0, 0, 0, 0)
-                x, y, w, h = box  # extracts the coordinates of the bounding box around the image
-                cx, cy = x + w // 2, y + h // 2  # calculates center point by adding 1/2 width to x and 1/2 height to y
-                img_height, img_width, _ = img.shape  # returns height, width of image
-                error_x = cx - img_width // 2  # caculate distance object is off-center horizontally
-                error_y = cy - img_height // 2  # caculate distance object is off-center vertically
-                me.send_rc_control(-int(error_y * 0.1), int(error_x * 0.1), 0, 0)  # adjusts drone using error values multiplied by a scaling factor (default .1)
-                #me.sleep(2)
-                print(distance)
-                print("Distance is less than 5 feet. Drone is landing")
-                me.land()
-                me.end()
-                #me.send_rc_control(0, 0, 0, 0)
-                #print(distance)
-                #me.land()
-            cvzone.cornerRect(img, box)
-            cv2.putText(img, f'{classNames[classId - 1].upper()} {round(conf * 100, 2)}',
-                        (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                while objName != "stop sign" or distance >= 130:
+                    # Send commands to the Tello to navigate to the current location
+                    drone.send_rc_control(relative_distance_cm[0], relative_distance_cm[1], 0, 0)
+                    print("about to sleep")
+                    drone.sleep(2)
+                    print("ending sleep")
+                else:
+                    print("check2")
+                    drone.send_rc_control(0, 0, 0, 0)
+                    x, y, w, h = rectangle  # extracts the coordinates of the bounding box around the image
+                    cx, cy = x + w // 2, y + h // 2  # calculates center point by adding 1/2 width to x and 1/2 height to y
+                    feed_height, feed_width, _ = feed.shape  # returns height, width of image
+                    error_x = cx - feed_width // 2  # caculate distance object is off-center horizontally
+                    error_y = cy - feed_height // 2  # caculate distance object is off-center vertically
+                    drone.send_rc_control(-int(error_y * 0.1), int(error_x * 0.1), 0, 0)  # adjusts drone using error values multiplied by a scaling factor (default .1)
+                    print(distance)
+                    print("Distance is less than 5 feet. Drone is landing")
+                    drone.land()
+                    drone.end()
+            cvzone.cornerRect(feed, rectangle)
+            cv2.putText(feed, f'{classObj[objId - 1].upper()} {round(flat * 100, 2)}',
+                        (rectangle[0] + 10, rectangle[1] + 30), cv2.FONT_HERSHEY_PLAIN,
                         1, (0, 255, 0), 2)
     except:
         pass
 
-    me.send_rc_control(0, 0, 0, 0)
+    drone.send_rc_control(0, 0, 0, 0)
 
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", feed)
     cv2.waitKey(1)
-
-
-
